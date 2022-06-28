34cc560f0f379a3f222c0aa39e78858845c742ca
KannapicmS <88262676+KannapicmS@users.noreply.github.com>
2022-06-28 18:55:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/KannapicmS/imsb.git /imsb
-cd /IMDb-Movie-Bot
+cd /imsb
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
